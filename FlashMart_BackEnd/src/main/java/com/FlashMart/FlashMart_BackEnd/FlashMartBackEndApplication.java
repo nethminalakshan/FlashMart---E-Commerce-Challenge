7d8041a0aaacafc5fed224328e68841ab6390732
Nethmina Lakshan Herath <nethminalakshan2018@gmail.com>
2025-09-17 15:54:42 +0530

    v2
    add backend file

---
+++ FlashMart_BackEnd/src/main/java/com/FlashMart/FlashMart_BackEnd/FlashMartBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.FlashMart.FlashMart_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashMartBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashMartBackEndApplication.class, args);
+	}
+
+}
---
+++ FlashMart_BackEnd/src/test/java/com/FlashMart/FlashMart_BackEnd/FlashMartBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlashMart.FlashMart_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashMartBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
